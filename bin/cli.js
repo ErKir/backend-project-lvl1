86b90b24a23d5fcf9132f9e2977deb1f52ed4394
kirill <kirillerohin@gmail.com>
2022-04-07 22:57:08 +0300

    refactor: change structure of the project

---
+++ bin/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+const askName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log('Hello ' + userName + '!');
+};
+
+export {
+  askName,
+};
