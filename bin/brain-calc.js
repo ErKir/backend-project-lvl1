5f8fc74f4b5102d87cc857500d46a69a2afee919
kirill <kirillerohin@gmail.com>
2022-04-21 23:50:00 +0300

    feat: brain-calc, generateRandomOperations

---
+++ bin/brain-calc.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+/* eslint-disable import/extensions */
+
+
+import readlineSync from 'readline-sync';
+import getRandomInt from '../src/generateRandomNumb.js';
+import getRandomOperation from '../src/generateRandomOperation.js';
+
+// minimum number, use only integer number
+const minNumb = 1;
+// maximum number, use only integer number
+const maxNumb = 100;
+
+// const randomNumb = getRandomInt(minNumb, maxNumb);
+let userName = '';
+const maximumTrueAnswers = 3;
+
+const askName = () => {
+  userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${  userName  }!`);
+};
+
+console.log("Welcome to the Brain Games!");
+askName();
+console.log('What is the result of the expression?');
+
+const gameEngine = (answerCount) => {
+  const randomNumb1 = getRandomInt(minNumb, maxNumb);
+  const randomNumb2 = getRandomInt(minNumb, maxNumb);
+  const operation = getRandomOperation();
+  if (answerCount === 0) {
+    console.log(`Congratulations, ${userName}!`);
+    return true;
+  }
+  let counter = answerCount;
+  console.log(`Question: ${randomNumb1} ${operation} ${randomNumb2}`);
+  const answer = readlineSync.question(`Your answer: `);
+  let trueAnswer;
+  switch (operation) {
+    case '+':
+      trueAnswer = randomNumb1 + randomNumb2;
+      break;
+    case '-':
+      trueAnswer = randomNumb1 - randomNumb2;
+      break;
+    case '*':
+      trueAnswer = randomNumb1 * randomNumb2;
+      break;
+    default:
+      console.log('case Error');
+  }
+  if (answer === trueAnswer) {
+    counter -= 1;
+    console.log('Correct!');
+    return gameEngine(counter);
+  }
+  console.log(`Uh, no! Is wrong answer;(.
+    Correct answer was ${trueAnswer}. Let 's try again, ${userName}!!`);
+  return true;
+};
+
+gameEngine(maximumTrueAnswers);
